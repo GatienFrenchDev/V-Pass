3d1b21ef245af5f7bd84aab383a75ff7c1998e9e
GatienFrenchDev <contact@gatiendev.tk>
2022-03-09 18:24:00 +0100

    667

--- html/js/renderer.js
+++ html/js/renderer.js
@@ -1,5 +1,8 @@
 const { ipcRenderer } = require('electron')
 
+/*
+get the username and put it in the right place
+*/
 ipcRenderer.send('username')
 ipcRenderer.on('reply', (event, data) =>{
     document.getElementById('username').innerText = data
