39282790f50846c8f29f0fb38ed2638354c2c788
GatienFrenchDev <gatieng2005@gmail.com>
2022-04-08 22:04:12 +0200

    avancement sur le systeme de stockage de mdp

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
+const util = require('util')
 const fs = require('fs')
 const sha256 = require('crypto-js/sha256')
 const aes = require('crypto-js/aes')
@@ -9,18 +10,19 @@ const aes = require('crypto-js/aes')
 app.commandLine.appendSwitch('no-sandbox')
 
 const default_config = {
-    path : __dirname+'/V-Pass_Config/pass.json',
-    main_hash : '0'
+    path: __dirname + '\\V-Pass_Config\\pass.json',
+    main_hash: '0'
 }
 
 // implementation of the config.json file, located in the "V-Pass_Config" folder
-fs.access('./V-Pass_Config', fs.constants.R_OK, async (err) =>{
-    if(err){
+fs.access('./V-Pass_Config', fs.constants.R_OK, async (err) => {
+    if (err) {
         fs.mkdirSync('./V-Pass_Config') // folder creation
-        fs.writeFileSync('./V-Pass_Config/pass.json','{}', { overwrite: false }) // pass file creation
-        fs.writeFileSync('./V-Pass_Config/config.json', JSON.stringify(default_config), { overwrite: false }) // config file creation
+        fs.writeFile('./V-Pass_Config/pass.json', '{}', { flag: "wx" }, () => {})
+        fs.writeFile('./V-Pass_Config/config.json', JSON.stringify(default_config), { flag: "wx" }, () => {})
     }
 })
+
 
 const config = require('./V-Pass_Config/config.json') // import config file
 const pass = require('./V-Pass_Config/pass.json') // import pass file
@@ -33,25 +35,25 @@ const loadMainWindow = () => {
         title: 'V-Pass',
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation : false
+            contextIsolation: false
         },
-        preload : path.join(__dirname, '/js/preload.js')
+        preload: path.join(__dirname, '/js/preload.js')
     })
     mainWindow.removeMenu()
-    if(config.main_hash == '0'){
-        mainWindow.loadFile(__dirname+`/html/inscription.html`)
+    if (config.main_hash == '0') {
+        mainWindow.loadFile(__dirname + `/html/inscription.html`)
     }
-    else{
-        mainWindow.loadFile(__dirname+`/html/login.html`)
+    else {
+        mainWindow.loadFile(__dirname + `/html/login.html`)
     }
-    // mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
 }
 
 app.on('ready', loadMainWindow)
 
 
-app.on('activate', () =>{
-    if(BrowserWindow.getAllWindows().length === 0){
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
         loadMainWindow()
     }
 })
@@ -62,10 +64,7 @@ app.on('window-all-closed', () => {
     }
 })
 
-ipcMain.on('username', (event, data) =>{
-    event.reply('reply', 'Lorem Ipsum')
-})
 
-ipcMain.on('pass', (event, data) =>{
+ipcMain.on('pass', (event, data) => {
     event.reply('reply', pass)
 })
